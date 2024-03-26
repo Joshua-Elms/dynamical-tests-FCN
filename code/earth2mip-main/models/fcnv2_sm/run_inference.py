12c558eabf6b19ee03375211f663c387db5e5732
Joshua Elms <joshua.elms111@gmail.com>
2024-03-25 20:06:15 -0400

    working on output analysis

--- code/earth2mip-main/models/fcnv2_sm/run_inference.py
+++ code/earth2mip-main/models/fcnv2_sm/run_inference.py
@@ -3,19 +3,19 @@ import datetime
 
 # make sure to set the environment variable MODEL_REGISTRY before running this script
 
-# run_custom_inference(
-#         data_source_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/initial_conditions/processed_ic_sets/test_data_source/",
-#         output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/test_output_0.nc",
+run_custom_inference(
+        data_source_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/initial_conditions/processed_ic_sets/test_data_source/",
+        output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/full_output.nc",
+        n_iters=52,
+        start_time=datetime.datetime(1970, 1, 1),
+        device="cpu",
+        vocal=True
+    )
+
+# run_simple_inference(
+#         output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/test_era5ic_output.nc",
 #         n_iters=1,
 #         start_time=datetime.datetime(1970, 1, 1),
 #         device="cpu",
 #         vocal=True
-#     )
-
-run_simple_inference(
-        output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/test_era5ic_output.nc",
-        n_iters=1,
-        start_time=datetime.datetime(1970, 1, 1),
-        device="cpu",
-        vocal=True
-)
+# )
---
+++ code/earth2mip-main/models/fcnv2_sm/run_inference.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -J fcnv2_sm_inference
+#SBATCH -p debug
+#SBATCH -o output.out
+#SBATCH -e log.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jmelms@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=32
+#SBATCH --time=01:00:00
+#SBATCH --mem=200G
+#SBATCH -A r00389
+
+export HDF5_USE_FILE_LOCKING=FALSE
+export NCCL_NET_GDR_LEVEL=PHB
+export MASTER_ADDR=$(hostname)
+
+set -x
+
+source activate ~/envs/earth2mip # environment for FCN
+
+echo "Beginning inference script"
+
+python run_inference.py
+
+echo "Inference script complete"
